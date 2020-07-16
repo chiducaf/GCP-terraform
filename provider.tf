a9c2acc9f9cab9055e6de78f0e5647bf803cdc32
chiducaf <58770319+chiducaf@users.noreply.github.com>
2020-07-16 17:03:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-credentials = "${file("env-pro-terraform-895932c0de6e.json")}"
+credentials = "${file("gkejenkinsx-6550e1dc828c.json")}"
 project     = "${var.project-name}"
 region      = "${var.region}"
 }
